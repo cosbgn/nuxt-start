c9e0bc3a6c1ba64a893050a7208bac6b5eb11432
Costantin Bignoli <cosbgn@gmail.com>
2022-10-05 12:32:23 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		Without Trailing slash:
+		<ul>
+			<li><a href="https://rx-gamma.vercel.app">https://rx-gamma.vercel.app</a> Works</li>
+			<li><a href="https://rx-gamma.vercel.app?foo=bar">https://rx-gamma.vercel.app?foo=bar</a> Works</li>
+			<li style="color:red;"><a href="https://rx-gamma.vercel.app?foo=bar+x">https://rx-gamma.vercel.app?foo=bar+x</a> Doesn't Work</li>
+		</ul>
+
+		With Trailing slash:
+		<ul>
+			<li><a href="https://rx-gamma.vercel.app/">https://rx-gamma.vercel.app/</a> Works</li>
+			<li><a href="https://rx-gamma.vercel.app/?foo=bar">https://rx-gamma.vercel.app/?foo=bar</a> Works</li>
+			<li style="color:red;"><a href="https://rx-gamma.vercel.app/?foo=bar+x">https://rx-gamma.vercel.app/?foo=bar+x</a> Doesn't Work</li>
+		</ul>
+
+		<p> The issue is in the + sign (I think)</p>
+	</div>
+</template>
